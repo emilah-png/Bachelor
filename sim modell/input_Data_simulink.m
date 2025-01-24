0971825aa01483fa2d40eff2f8059ca86af2094f
Kristinebp <kristinebp@uia.no>
2025-01-24 15:01:20 +0100

    Data fra fil til simscape

---
+++ sim modell/input_Data_simulink.m
@@ -0,0 +1,16 @@
+clc; clear; close all; 
+
+% Get data from file
+load('SinusHorisontalStartMedFlow.mat')
+
+% Extract data and save
+Run = data{17}.extractTimetable;
+Control_signal = Run.("<fU_G_OPCfU>");
+
+% 
+time = Run.Time;
+time = time2num(time);
+
+inputData = timeseries(Control_signal,time);
+save("inputData.mat","inputData","-v7.3");
+
