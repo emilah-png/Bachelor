2643272a5b13644b2b5306e590cb78b17ef52c8b
emilah-png <emilah@uia.no>
2025-04-11 10:15:09 +0200

    laget bilder til subsystem

--- matlab/ValveResponseTimePlot_data.m
+++ matlab/ValveResponseTimePlot_data.m
@@ -1,4 +1,4 @@
-clc; clear; close all;
+clc; clear;
 Jib_data=readtable('CSV plotter\Adjusted_JibValveResponseTime_3Pressure_010425.csv');
 Main_data=readtable('CSV plotter\Adjusted_MainValveResponseTime_3Pressure_010425.csv');
 
@@ -16,22 +16,22 @@ RArea_Jib = pi/4*(D_Jib^2-r_Jib^2);
 u_init = 0.2;
 u_end = 0.8;
 
-for i = 1:4000
-    Main_data_130(i,:) = Main_data(i,:);
-end
-for i = 4000:8000
-    Main_data_140(i,:) = Main_data(i,:);
-end
+% for i = 1:4000
+%     Main_data_130(i,:) = Main_data(i,:);
+% end
+% for i = 4000:8000
+%     Main_data_140(i,:) = Main_data(i,:);
+% end
 for i = 8000:11000
     Main_data_150(i,:) = Main_data(i,:);
 end
 
-for i = 1:4000
-    Jib_data_130(i,:) = Jib_data(i,:);
-end
-for i = 8000:12000
-    Jib_data_140(i,:) = Jib_data(i,:);
-end
+% for i = 1:4000
+%     Jib_data_130(i,:) = Jib_data(i,:);
+% end
+% for i = 8000:12000
+%     Jib_data_140(i,:) = Jib_data(i,:);
+% end
 for i = 13000:17000
     Jib_data_150(i,:) = Jib_data(i,:);
 end
